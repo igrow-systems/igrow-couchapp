1fcc0bbc451d7e6b8902a561a7fa3e694ca58885
Jeremy Reeve <jeremy.reeve@opengpssignal.com>
2015-03-02 18:31:56 +0000

    Fix some bugs.  Works now mostly.  Need to tidy up output.

--- scripts/seeds-csv2json.py
+++ scripts/seeds-csv2json.py
@@ -30,18 +30,21 @@ class ParserState:
 
     def parsed_header(self):
         if (self.current_state == ParserState.reading_header):
+            print "Transition ", self.current_state, " -> ", ParserState.reading_supplier_name
             self.current_state = ParserState.reading_supplier_name
         else:
             illegal_transition()
 
     def parsed_supplier_name(self):
         if (self.current_state == ParserState.reading_supplier_name):
+            print "Transition ", self.current_state, " -> ", ParserState.reading_supplier_data
             self.current_state = ParserState.reading_supplier_data
         else:
             illegal_transition()
 
     def parsed_supplier_data(self):
         if (self.current_state == ParserState.reading_supplier_data):
+            print "Transition ", self.current_state, " -> ", ParserState.reading_supplier_name
             self.current_state = ParserState.reading_supplier_name
         else:
             illegal_transition()
@@ -56,34 +59,51 @@ class ParserState:
 
 preamble_text = 'Vegetable Seed Order'
 supplier_column = 0
+total_column = 8
+total_text = 'Total:'
+
 current_state = ParserState()
 seeds = []
+preamble_read = False
+num_columns = -1
 
 with open(sys.argv[1], 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
 
     for row in reader:
+        # skip the preamble
+        if (not preamble_read):
+            if (row[0] == preamble_text):
+                preamble_read = True
+                continue
+
+        if (num_columns == -1):
+            num_columns = len(row)
+            print num_columns, " columns."
+
+        # catch all blank rows
+        num_blank_cols = sum(column == '' for column in row)
+        if (num_blank_cols >= num_columns):
+            continue
+
+        print num_blank_cols, ", ".join(row)
 
         if (current_state.get_state() == ParserState.reading_header):
 
-            # skip the preamble
-            if (row[0] == preamble_text):
-                num_columns = len(row[0])
-                continue
-
             fields = row;
             
-            # test if we need to change state
-            #if (sum(column == None for column in row) == num_columns):
             current_state.parsed_header()
 
         elif (current_state.get_state() == ParserState.reading_supplier_name):
 
             current_supplier = row[0]
-
             current_state.parsed_supplier_name()
 
         elif (current_state.get_state() == ParserState.reading_supplier_data):
+
+            if (row[total_column] == total_text):
+                current_state.parsed_supplier_data()
+                continue
 
             tuple_list = []
             for i in range(0,len(fields) - 1):
